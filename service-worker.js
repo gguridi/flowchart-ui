1c7eb5b221eb0efe90e77e8a62eeda97b85d03bf
gguridi <gguridi@users.noreply.github.com>
2020-09-05 10:37:26 +0000

    deploy: 0a01e6195c04be386bdfda7d7e2c394bd7b37e59

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flowchart-ui/precache-manifest.913164fcf250f5b77bc65ecdb6884c90.js"
+  "/flowchart-ui/precache-manifest.dd1997c2604b9a6787f6b07bcb840fe7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flowchart-ui/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
