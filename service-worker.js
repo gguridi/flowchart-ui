841d510551d7994124f87b32ec5477ed1f6f5606
gguridi <gguridi@users.noreply.github.com>
2020-09-13 16:35:03 +0000

    deploy: 94cd5b9a3a86321ff241c2236dc2f160aadf7c5f

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flowchart-ui/precache-manifest.d7da2ea2237f66a938163ca8c7d57912.js"
+  "/flowchart-ui/precache-manifest.3de168e5b3fbc85731aff5df9b668208.js"
 );
 
 self.addEventListener('message', (event) => {
